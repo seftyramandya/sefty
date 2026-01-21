649b83a49ba9335e2a924c06ef0d6e2533b102ac
seftyramandya <ramandiasefti.com>
2026-01-21 10:03:29 +0700

    up biodata

--- pembelajaran.php/biodata.php
+++ pembelajaran.php/biodata.php
@@ -1,4 +1,8 @@
 <?php
-echo "Nama:Sefty<br>" ;
-echo "Kelas:XI TJKT 2" ;
+echo "Nama:Sefty Ramandya Azzuhra<br>" ;
+echo "Kelas:XI TJKT 2<br>" ;
+echo "Tempat Tanggal Lahir:20 september 2008<br>" ;
+echo "Alamat:Jl. Bojong citepus RT04/RW09<br>";
+echo "Instagram:sftyrdyzhr_<br>";
+echo "Hobi:Membaca, mendengarkan musik, memasak, menonton drakor<br>" ;
 ?>
