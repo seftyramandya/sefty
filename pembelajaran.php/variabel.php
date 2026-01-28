255bd69577ee0b2f4b54592b68e8bbfda317311c
seftyramandya <ramandiasefti.com>
2026-01-28 08:57:39 +0700

    up

---
+++ pembelajaran.php/variabel.php
@@ -0,0 +1,58 @@
+<?php
+
+// tipe data char (karakter)
+$jenis_kelamin ='P' ;
+
+// tipe data string (teks)
+$nama_lengkap = "Sefty Ramandya Azzuhra";
+
+// tipe data integer
+$umur = 17;
+
+// tipe data float
+$berat = 52;
+
+// tipe data float
+$tinggi = 154;
+
+
+echo "Nama: $nama_lengkap<br>";
+echo "Jenis Kelamin: $jenis_kelamin<br>";
+echo "Umur: $umur tahun<br>";
+echo "berat badan: $berat kg<br>";
+echo "tinggi badan: $tinggi cm<br>";
+
+
+$a = 5;
+$b = 2;
+
+// penjumlahan
+$c = $a + $b;
+echo "$a + $b = $c";
+echo "<hr>";
+
+// pengurangan
+$c = $a - $b;
+echo "$a - $b = $c";
+echo "<hr>";
+
+// Perkalian
+$c = $a * $b;
+echo "$a * $b = $c";
+echo "<hr>";
+
+// Pembagian
+$c = $a / $b;
+echo "$a / $b = $c";
+echo "<hr>";
+
+// Sisa bagi
+$c = $a % $b;
+echo "$a % $b = $c";
+echo "<hr>";
+
+// Pangkat
+$c = $a ** $b;
+echo "$a ** $b = $c";
+echo "<hr>";
+?>
